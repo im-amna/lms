20a317767b9c9399ffe165b99c8530412204a413
Amna Naveed <amna74345@gmail.com>
2026-07-01 07:39:17 -0700

    Add demo routes to courseRouter

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { getAllCourse, getCourseId } from "../controllers/courseController.js";
+import {
+  getAllCourse,
+  getCourseId,
+  getDemoCourses,
+} from "../controllers/courseController.js";
 
 const courseRouter = express.Router();
 
+// Normal Routes
 courseRouter.get("/all", getAllCourse);
 courseRouter.get("/:id", getCourseId);
+
+// Demo Routes (No Login Required)
+courseRouter.get("/demo/all", getDemoCourses);
 
 export default courseRouter;
